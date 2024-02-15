9e773c79946da3edac2c3d64ca7ce42b66db59ea
Allison Randel <allisonrandel@allisons-mbp-2.attlocal.net>
2024-02-15 15:17:50 -0600

    puppy data with useState complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
 
   return (
     <>
-      <div>
-        
+      <div className ="App">
+        {puppies.map((puppy) => {
+          return<p key={puppy.id} > {puppy.name}</p>;
+        })
+        }
       </div>
     </>
-  )
+  );
 }
 
 export default App
